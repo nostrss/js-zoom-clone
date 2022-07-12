275f1e39c2d8c652a10d49a1842ec87483a6fe39
nostrss <jintagi@gmail.com>
2022-07-12 16:22:30 +0900

    end section 1-5

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,20 @@
 // 프론트 코드
-alert('hi');
+
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// 연결이 됐을때
+socket.addEventListener('open', () => {
+  console.log('connected to server ⭐️');
+});
+
+socket.addEventListener('message', (message) => {
+  console.log('got a message📝', message.data);
+});
+
+socket.addEventListener('close', () => {
+  console.log('Disconnect server❌');
+});
+
+setTimeout(() => {
+  socket.send('hello from the browser!');
+}, 10000);
